c9501a9171183c509ab626b4dcf42935a768425d
mili81 <milisenta81@gmail.com>
2021-11-05 22:04:26 +0200

    almost final

--- src/com/company/Database.java
+++ src/com/company/Database.java
@@ -8,38 +8,52 @@ public class Database {
     static final String DB_URL = "jdbc:mysql://group1db.ckfcq92zr1jy.eu-west-2.rds.amazonaws.com/shoes";
 
     static final String USER = "admin";
-    static final String PASSWORD = "Shoes_1234";
+
+
     static Connection conn = null;
     static Statement stmt = null;
 
-    public static void insertValues() throws SQLException {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Name:");
-        String name = input.next();
-        System.out.println("Size:");
-        int size = input.nextInt();
-        System.out.println("Colour:");
-        String colour = input.next();
-        System.out.println("Brand:");
-        String brand = input.next();
+    public static void repeat() throws SQLException{
+       Scanner input = new Scanner(System.in);
+        System.out.println("How many rows You will enter?");
+        int answer = input.nextInt();
+        int count = 0;
+        do {
+            insertValues();
+            count++;
+        }while(count<answer);
+        }
+
+            public static void insertValues () throws SQLException {
+                Scanner input = new Scanner(System.in);
+                System.out.println("Name:");
+                String name = input.next();
+                System.out.println("Size:");
+                int size = input.nextInt();
+                System.out.println("Colour:");
+                String colour = input.next();
+                System.out.println("Brand:");
+                String brand = input.next();
 
 
-        String sql = "INSERT INTO Shoes(name,size,colour,brand) VALUES (+" +
-               "'" + name + "'," + size + ",'" + colour + "','" + brand + "')";
-        stmt.execute(sql);
-    }
+                String sql = "INSERT INTO Shoes(name,size,colour,brand) VALUES (+" +
+                        "'" + name + "'," + size + ",'" + colour + "','" + brand + "')";
+                stmt.execute(sql);
+            }
 
 
-    public static void main(String[] args) throws SQLException {
+
+    public static void main(String[] args){
         try {
 
             System.out.println("Connecting to database...");
-            conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            conn = DriverManager.getConnection(DB_URL, USER,Pass.getPassword());
             System.out.println("Creating statement...");
 
             //Create statement object
             stmt = conn.createStatement();
-            insertValues();
+            //insertValues();
+            repeat();
 
         } catch (SQLException sqlException) {
             System.out.println("Error:" + sqlException.getMessage());
@@ -49,6 +63,7 @@ public class Database {
                 stmt.close();
                 conn.close();
             } catch (SQLException ex) {
+                System.out.println("Got error and connection closed." + ex.getMessage());
             }
         }
 
---
+++ src/com/company/Pass.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Pass {
+    final static String PASSWORD = "Shoes_1234";
+
+    public static String getPassword() {
+        return PASSWORD;
+    }
+}
