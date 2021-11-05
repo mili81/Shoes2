4d4ce08a31f7557ee699e2420536c75791b7fb7a
mili81 <milisenta81@gmail.com>
2021-11-05 23:27:53 +0200

    deleted commented lines

--- src/com/company/Database.java
+++ src/com/company/Database.java
@@ -1,49 +1,47 @@
 package com.company;
+
 import java.sql.*;
 import java.util.Scanner;
 
 
 public class Database {
 
-        static final String DB_URL = "jdbc:mysql://group1db.ckfcq92zr1jy.eu-west-2.rds.amazonaws.com/shoes";
-
-        //static final String USER = "admin";
+    static final String DB_URL = "jdbc:mysql://group1db.ckfcq92zr1jy.eu-west-2.rds.amazonaws.com/shoes";
 
 
     static Connection conn = null;
     static Statement stmt = null;
 
-    public static void repeat() throws SQLException{
-       Scanner input = new Scanner(System.in);
+    public static void repeat() throws SQLException {
+        Scanner input = new Scanner(System.in);
         System.out.println("How many rows You will enter?");
         int answer = input.nextInt();
         int count = 0;
         do {
             insertValues();
             count++;
-        }while(count<answer);
-        }
+        } while (count < answer);
+    }
 
-            public static void insertValues () throws SQLException {
-                Scanner input = new Scanner(System.in);
-                System.out.println("Name:");
-                String name = input.next();
-                System.out.println("Size:");
-                int size = input.nextInt();
-                System.out.println("Colour:");
-                String colour = input.next();
-                System.out.println("Brand:");
-                String brand = input.next();
-
-
-                String sql = "INSERT INTO Shoes(name,size,colour,brand) VALUES (+" +
-                        "'" + name + "'," + size + ",'" + colour + "','" + brand + "')";
-                stmt.execute(sql);
-            }
+    public static void insertValues() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Name:");
+        String name = input.next();
+        System.out.println("Size:");
+        int size = input.nextInt();
+        System.out.println("Colour:");
+        String colour = input.next();
+        System.out.println("Brand:");
+        String brand = input.next();
 
 
+        String sql = "INSERT INTO Shoes(name,size,colour,brand) VALUES (+" +
+                "'" + name + "'," + size + ",'" + colour + "','" + brand + "')";
+        stmt.execute(sql);
+    }
 
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner name = new Scanner(System.in);
         System.out.println("Enter username:");
         String username = name.next();
@@ -58,7 +56,7 @@ public class Database {
 
             //Create statement object
             stmt = conn.createStatement();
-            //insertValues();
+
             repeat();
 
         } catch (SQLException sqlException) {
